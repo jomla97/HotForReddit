a8d15913d89cb8d9926a0ab74833dfffaa42da7b
jomla97 <johannes.nyman97@hotmail.com>
2016-12-25 22:46:37 +0100

    Basic layout and structure

--- app/components/styles.js
+++ app/components/styles.js
@@ -3,10 +3,66 @@ import {StyleSheet} from 'react-native';
 
 const primaryColor = "#ff9000";
 const primaryColorDark = "#e87f00";
+const background = "#e8e8e8";
+const primaryFontColor = "#545454";
 
 module.exports = StyleSheet.create({
   toolbar: {
     backgroundColor: primaryColor,
     height: 56
+  },
+  drawer: {
+    backgroundColor: background
+  },
+  contentContainer: {
+    backgroundColor: "#e8e8e8",
+    flex: 1,
+    padding: 5,
+    //alignItems: "center",
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    margin: 5,
+    //padding: 5,
+    flex: 1,
+    elevation: 2
+  },
+  previewImage: {
+    height: 120,
+    width: null,
+    resizeMode: "cover",
+    flex: 1
+  },
+  postTitlePreview: {
+    margin: 5,
+    fontSize: 16
+  },
+  buttonContainer: {
+    backgroundColor: "#f9f9f9",
+    padding: 10,
+    flexDirection: "row-reverse"
+  },
+  icon: {
+    color: primaryFontColor
+  },
+  voteButton: {
+    paddingLeft: 5,
+    paddingRight: 5
+  },
+  otherButton: {
+    paddingLeft: 8,
+    paddingRight: 8,
+    paddingTop: 5
+  },
+  voteButtonContainer: {
+    flex: 1,
+    flexDirection: "row-reverse",
+  },
+  otherButtonContainer: {
+    flex: 4,
+    flexDirection: "row"
+  },
+  voteCount: {
+    paddingTop: 5
   }
 });
--- index.android.js
+++ index.android.js
@@ -4,10 +4,13 @@ import {
   StyleSheet,
   Text,
   View,
+  ScrollView,
   DrawerLayoutAndroid,
   TouchableHighlight,
   ToolbarAndroid,
   StatusBar,
+  Image,
+  Alert,
 } from 'react-native';
 
 import {
@@ -15,72 +18,116 @@ import {
   Button,
 } from 'native-base';
 
-import Icon from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Card} from 'react-native-material-design';
 import styles from './app/components/styles.js';
 import GLOBALS from './app/components/globals.js';
 
-class HotForReddit extends Component {
+var HotForReddit = React.createClass({
 
-	static childContextTypes = {
+	/*
+  static childContextTypes = {
 		drawer: React.PropTypes.object,
 		navigator: React.PropTypes.object
 	};
+  */
 
-	constructor(props) {
-		super(props);
+  /*
+	constructor: function(props) {
+		//super(props);
 		this.state = {
 			drawer: null,
 			navigator: null
 		};
-	}
-
-  get_top_posts(){
+	},
+  */
+  //REDDIT API
+  get_top_posts: function(){
     fetch("https://www.reddit.com/r/todayilearned/top.json?count=N&after=t3_XXXXX")
     .then((response) => response.json())
     .then((responseJson) => {
       return responseJson;
     })
     .done();
-  };
+  },
+  vote: function(){
+    Alert.alert("vote");
+  },
+  comments: function(){
+    Alert.alert("comments");
+  },
+  share: function(){
+    Alert.alert("share");
+  },
 
-  onActionSelected(position){
-    if (position === 0) { // index of 'drawer'
-      this.drawer.openDrawer();
-    }
-  }
 
-  render(){
+  _openDrawer: function(){
+    this.refs["drawer"].openDrawer();
+    //if(position === 0){
+    //  this.refs["drawer"].openDrawer();
+    //}
+  },
+  render: function(){
     var navigationView = (
       <View style={{flex: 1, backgroundColor: '#fff'}}>
         {/*drawer/navigation content*/}
 
       </View>
-    );
+    )
 
     return (
       <DrawerLayoutAndroid
         drawerWidth={300}
         drawerPosition={DrawerLayoutAndroid.positions.Left}
-        renderNavigationView={() => navigationView}>
+        renderNavigationView={() => navigationView}
+        ref="drawer"
+      >
         <StatusBar
           backgroundColor={GLOBALS.primaryColorDark}
           barStyle="light-content"
         />
-        <ToolbarAndroid
+        <Icon.ToolbarAndroid
           //logo={require('./app_logo.png')}
           //title="Hot for Reddit"
-          actions={[{title: 'Drawer', icon: require("./app/res/menu.png"), show: 'always'}]}
-          onActionSelected={this.onActionSelected}
+          //actions={[{title: 'Drawer', name: require("./app/res/menu.png"), show: 'always'}]}
+          //onActionSelected={this.onActionSelected}
+          navIconName="menu"
+          onIconClicked={this._openDrawer}
+          iconColor="#FFFFFF"
           style={styles.toolbar}
         />
-        <View style={{flex: 1, alignItems: 'center'}}>
+        <ScrollView style={styles.contentContainer}>
           {/*main content*/}
-          <Text>Some text..</Text>
-        </View>
+          <View style={styles.card}>
+            <Image source={require("./app/res/previewImage.jpg")} style={styles.previewImage}/>
+            <View>
+              <Text style={styles.postTitlePreview}>Picture of a really cute kitty</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+              <View style={styles.voteButtonContainer}>
+                <TouchableHighlight style={styles.voteButton} onPress={this.vote}>
+                  <Icon name="keyboard-arrow-up" size={30} style={styles.icon}/>
+                </TouchableHighlight>
+                <Text style={styles.voteCount}>2193</Text>
+                <TouchableHighlight style={styles.voteButton} onPress={this.vote}>
+                  <Icon name="keyboard-arrow-down" size={30} style={styles.icon}/>
+                </TouchableHighlight>
+              </View>
+
+              <View style={styles.otherButtonContainer}>
+                <TouchableHighlight style={styles.otherButton} onPress={this.share}>
+                  <Icon name="share" size={20} style={styles.icon}/>
+                </TouchableHighlight>
+                <TouchableHighlight style={styles.otherButton} onPress={this.comments}>
+                  <Icon name="comment" size={20} style={styles.icon}/>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </DrawerLayoutAndroid>
     );
   }
-}
+})
 
 AppRegistry.registerComponent('HotForReddit', () => HotForReddit);
